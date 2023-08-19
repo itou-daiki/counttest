e3661dcea71fefda3b953ec4267751afdb2f4739
itou-daiki <141300506+itou-daiki@users.noreply.github.com>
2023-08-19 02:07:14 +0000

    あ

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,15 +16,20 @@ def detect_faces(img):
         cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
     return img, len(faces)
 
-st.title("画像から人数を数える")
+# タイトルと説明を追加
+st.title("顔検出アプリ")
+st.write("画像をアップロードすると、その画像から顔を検出し、検出された顔の数を表示します。")
 
-uploaded_file = st.file_uploader("画像をアップロードしてください", type=['jpg', 'jpeg', 'png'])
+# 画像アップロードの部分を中央に配置
+uploaded_file = st.file_uploader("画像をアップロードしてください", type=['jpg', 'jpeg', 'png'], align="center")
 
 if uploaded_file is not None:
+    st.write("アップロードされた画像：")
     image = Image.open(uploaded_file)
+    st.image(image, caption="アップロードされた画像", use_column_width=True)
+
     image_np = np.array(image)
-    
     processed_image, num_faces = detect_faces(image_np)
 
     st.write(f"検出された人数: {num_faces}")
-    st.image(processed_image, channels="BGR", use_column_width=True)
+    st.image(processed_image, caption="顔検出後の画像", channels="BGR", use_column_width=True)
