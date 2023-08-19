2224fc79ef03ade5b0b0a2669abba5c9af6991b2
itou-daiki <141300506+itou-daiki@users.noreply.github.com>
2023-08-19 02:41:18 +0000

    あ

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,35 +8,6 @@ import pytz
 
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 
-def detect_faces(img):
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5)
-    for (x, y, w, h) in faces:
-        cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
-    return img, len(faces)
-
-def get_exif_data(image):
-    return {ExifTags.TAGS[k]: v for k, v in image._getexif().items() if k in ExifTags.TAGS and isinstance(v, (str, bytes))}
-
-def get_geotagging(exif):
-    if 'GPSInfo' in exif:
-        return {ExifTags.GPSTAGS[k]: v for k, v in exif['GPSInfo'].items() if k in ExifTags.GPSTAGS}
-    return None
-
-def save_to_csv(num_faces):
-    current_datetime_jst = datetime.utcnow().astimezone(pytz.timezone('Asia/Tokyo'))
-    new_data = pd.DataFrame([{
-        'Date': current_datetime_jst.strftime('%Y-%m-%d'),
-        'Time': current_datetime_jst.strftime('%H:%M:%S'),
-        'Number of Faces': num_faces
-    }])
-    try:
-        df = pd.read_csv('data.csv')
-        df = pd.concat([df, new_data], ignore_index=True)
-    except FileNotFoundError:
-        df = new_data
-    df.to_csv('data.csv', index=False)
-
 st.title("顔検出アプリ")
 st.write("画像をアップロードすると、その画像から顔を検出し、検出された顔の数とExifデータを表示します。")
 
@@ -47,13 +18,18 @@ if uploaded_file:
     st.image(image, caption="アップロードされた画像", use_column_width=True)
 
     image_np = np.array(image)
-    processed_image, num_faces = detect_faces(image_np)
-    st.write(f"検出された人数: {num_faces}")
-    st.image(processed_image, caption="顔検出後の画像", channels="BGR", use_column_width=True)
+    gray = cv2.cvtColor(image_np, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5)
+    for (x, y, w, h) in faces:
+        cv2.rectangle(image_np, (x, y), (x+w, y+h), (255, 0, 0), 2)
+    st.write(f"検出された人数: {len(faces)}")
+    st.image(image_np, caption="顔検出後の画像", channels="BGR", use_column_width=True)
 
-    exif_data = get_exif_data(image)
+    exif_data = {ExifTags.TAGS[k]: v for k, v in image._getexif().items() if k in ExifTags.TAGS and isinstance(v, (str, bytes))}
     capture_time = exif_data.get("DateTimeOriginal", None)
-    geotags = get_geotagging(exif_data)
+    geotags = None
+    if 'GPSInfo' in exif_data:
+        geotags = {ExifTags.GPSTAGS[k]: v for k, v in exif_data['GPSInfo'].items() if k in ExifTags.GPSTAGS}
 
     if capture_time:
         st.write(f"撮影日時: {capture_time}")
@@ -62,5 +38,16 @@ if uploaded_file:
     else:
         st.write("ExifデータにGPS情報は見つかりませんでした。")
 
-    save_to_csv(num_faces)
+    current_datetime_jst = datetime.utcnow().astimezone(pytz.timezone('Asia/Tokyo'))
+    new_data = pd.DataFrame([{
+        'Date': current_datetime_jst.strftime('%Y-%m-%d'),
+        'Time': current_datetime_jst.strftime('%H:%M:%S'),
+        'Number of Faces': len(faces)
+    }])
+    try:
+        df = pd.read_csv('data.csv')
+        df = pd.concat([df, new_data], ignore_index=True)
+    except FileNotFoundError:
+        df = new_data
+    df.to_csv('data.csv', index=False)
     st.download_button("CSVをダウンロード", data=open('data.csv', 'rb').read(), file_name="data.csv", mime="text/csv")
